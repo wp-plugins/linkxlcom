03fcb377a9a4dc60851b433ba0c3f1ba37577a88
mzawadzinski <mzawadzinski@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-07 10:01:54 +0000

    closes #733
    git-svn-id: https://plugins.svn.wordpress.org/linkxlcom/trunk@261038 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ContractsList.php
+++ ContractsList.php
@@ -9,7 +9,9 @@ class ContractsList {
 
     private $contract_list;
 
-    public function  __construct()
+    private $separate_tags_bad = array('a', 'textarea', 'option');
+
+    public function __construct()
     {
         $json_config = getJSONResponse();
         $contracts = $json_config->getContracts();
@@ -29,31 +31,127 @@ class ContractsList {
 
     public function parse($content)
     {
-        $content = ' '.$content;
-        $content_s = strtolower($content);
-        foreach($this->contract_list as $j => $contract){
-            $keyword_s = strtolower($contract['keyword']);
-            $l = strlen($keyword_s)+2;
+        $content = ' '.$content.' ';
 
-            $content_length = strlen($content);
+        if(count($this->contract_list)>0){
 
-            for($i=0;$i+$l<$content_length;$i++){
-                $str = substr($content_s, $i, $l);
+            foreach($this->contract_list as $j => $contract){
+                $keyword_s = strtolower($contract['keyword']);
+                $l = strlen($keyword_s)+2;
 
-                if(preg_match('/\W{1}'.$keyword_s.'\W{1}/', $str)){
-                    $content_temp = substr($content, 0, $i+1);
-                    $content_temp .= sprintf('<a href="%s" target="new">%s</a>', $contract['advertiser_url'], substr($content, $i+1, $l-2));
-                    $content_temp .= substr($content, $i+1+strlen($contract['keyword']));
+                $content_length = strlen($content);
 
-                    unset($this->contract_list[$j]);
-                    $content = $content_temp;
+                for($i=0;$i+$l<$content_length;$i++){
+                    $str = substr($content, $i, $l);
 
-                    break;
+                    if(preg_match('/\W{1}'.$keyword_s.'\W{1}/', strtolower($str))){
+                        if(!$this->can_change_keyword($i+1, $i+$l-2, $content, $str)){
+
+                            continue;
+                        }
+
+                        $content_temp = substr($content, 0, $i+1);
+                        $content_temp .= sprintf('<a href="%s" target="new">%s</a>', $contract['advertiser_url'], substr($content, $i+1, $l-2));
+                        $content_temp .= substr($content, $i+1+strlen($contract['keyword']));
+
+                        unset($this->contract_list[$j]);
+                        $content = $content_temp;
+
+                        break;
+                    }
                 }
             }
         }
 
         return substr($content, 1);
+    }
+    /**
+     * $t_b_s_p - tag begin start position in content
+     * $t_b_e_p - tag begin end position in content
+     * $t_e_s_p - tag end start position in content
+     * $t_e_e_p - tag end end position in content
+     *
+     * @param int $start_postion
+     * @param int $end_postion
+     * @param string $content
+     * @param string $key
+     * @return boolean
+     */
+    private function can_change_keyword($start_postion, $end_postion, $content, $key)
+    {
+        $bad_tags = implode('|', $this->separate_tags_bad);
+
+        $pattern_1 = "/<(".$bad_tags.")*\/(".$bad_tags.")>/";
+
+        $content_length = strlen($content);
+
+        $t_b_s_p = $this->str_last_pos($content, '<', $start_postion);
+
+        $t_b_e_p = $this->str_last_pos($content, '>', $start_postion);
+
+        $t_e_s_p = $this->str_first_pos($content, '<', $end_postion);
+
+        $t_e_e_p = $this->str_first_pos($content, '>', $end_postion);
+
+        if(($t_b_s_p === false && $t_b_e_p === false) || ($t_e_s_p === false && $t_e_e_p === false)){
+            return true;
+        }
+
+        if($t_b_s_p !== false && $t_b_e_p !== false && $t_e_s_p !== false && $t_e_e_p !== false && ($t_b_s_p < $t_b_e_p) && ($t_e_s_p < $t_e_e_p)){
+            return !preg_match($pattern_1, $this->str_cut($t_b_s_p, $t_e_e_p, $content));
+        }
+
+        if($t_b_s_p !== false && $t_e_e_p !== false && ($t_b_s_p > $t_b_e_p) && ($t_e_s_p > $t_e_e_p)){
+            return strip_tags($this->str_cut($t_b_s_p, $t_e_e_p, $content)) != '';
+        }
+
+
+        return true;
+    }
+
+    private function str_last_pos($str, $search, $end_pos)
+    {
+        $l = strlen($search);
+
+        $i = $end_pos;
+        $str_t = '';
+        while($i>=0){
+            $str_t = $str[$i].$str_t;
+            if($str[$i] == $search){
+                $this->ss[] = $str_t;
+                return $i;
+            }
+            $i--;
+        }
+
+        return false;
+    }
+
+    private function str_first_pos($str, $search, $start_pos)
+    {
+        $l = strlen($search);
+		$str_length = strlen($str);
+        $i = $start_pos;
+
+        while($i<$str_length){
+            if($str[$i] == $search){
+                return $i;
+            }
+            $i++;
+        }
+
+        return false;
+    }
+
+    private function str_cut($start, $end, $text)
+    {
+        $str = '';
+
+	for($i=$start;$i<=$end;$i++){
+            $str .= $text[$i];
+	}
+
+	return $str;
     }
 }
 ?>
