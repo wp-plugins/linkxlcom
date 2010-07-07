c8ff5dae39972cffc676735f7d5ed9925fcf2dc8
mzawadzinski <mzawadzinski@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-07 10:02:24 +0000

    closes #733
    git-svn-id: https://plugins.svn.wordpress.org/linkxlcom/trunk@261041 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Handlers.php
+++ Handlers.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://linkxl.com
  * Settings URI: 
  * Description: LinkXL enables Wordpress bloggers to easily sell text link advertisting within existing content.
- * Version: 2.1
+ * Version: 2.2
  * Author: LinkXL
  * Author URI: http://linkxl.com
  */
